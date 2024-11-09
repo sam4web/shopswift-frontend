cdecb2bccb69b5ec58ec55add4b014497e063ea0
Sijal Manandhar <sijal.m06@gmail.com>
2024-11-09 21:24:15 +0545

    project setup with vite & tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="bg-slate-900 h-screen px-7 py-3">
+      <h1 className="text-3xl text-white">Hello, World!</h1>
+    </main>
+  );
+};
+
+export default App;
