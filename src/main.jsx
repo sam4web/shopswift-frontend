38da3df879a7cbd411448761ebdeced68f4c3ebd
Sijal Manandhar <sijal.m06@gmail.com>
2024-11-12 16:11:45 +0545

    `disableReactDevTools` for production env

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "@/assets/styles/main.scss";
 import App from "@/App.jsx";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (import.meta.env.VITE_NODE_ENV === "production")
+  disableReactDevTools();
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
