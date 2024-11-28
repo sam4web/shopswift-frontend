4716b5ad9291db7a9b4cd137b16f3607e24db354
Sijal Manandhar <sijal.m06@gmail.com>
2024-11-28 20:46:01 +0545

    removed spinner on page load

--- src/components/product/PrefetchProducts.jsx
+++ src/components/product/PrefetchProducts.jsx
@@ -1,13 +1,11 @@
 import { useDispatch } from "react-redux";
 import { Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { fetchProductsQuery } from "@/features/product/productThunks.js";
-import Spinner from "@/components/common/Spinner.jsx";
 import { toast } from "react-toastify";
 
 const PrefetchProducts = () => {
   const dispatch = useDispatch();
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -19,15 +17,10 @@ const PrefetchProducts = () => {
       } catch (err) {
         toast.dismiss();
         toast.error(err);
-      } finally {
-        toast.dismiss();
-        setLoading(false);
       }
     };
     fetchProducts();
   }, [dispatch]);
-
-  if (loading) return <Spinner />;
 
   return <Outlet />;
 };
